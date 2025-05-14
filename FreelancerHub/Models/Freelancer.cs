c53075d0325f911b36b4f53e39155cdbcf42f2e3
izzat amir <a189571@siswa.ukm.edu.my>
2025-05-14 21:43:51 +0800

    Add project files.

---
+++ FreelancerHub/Models/Freelancer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreelancerHub.Models
+{
+    public class Freelancer
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+
+        public string PhoneNo { get; set; }
+        public string Skillsets { get; set; }
+        public string Hobbies { get; set; }
+    }
+}
